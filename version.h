8a0489c4f4f8208c03da01d88af5087650bd40d2
Ivan Shatsky <ivan@shatsky.net>
2022-12-02 23:35:21 +0200

    Support for far2l extensions, backported from https://github.com/unxed/putty4far2l

--- version.h
+++ version.h
@@ -8,6 +8,6 @@
  * default stuff used for local development runs of 'make'.
  */
 
-#define TEXTVER "Unidentified build"
+#define TEXTVER "Unidentified build with far2l extensions support"
 #define SSHVER "-Unidentified-Local-Build"
 #define BINARY_VERSION 0,0,0,0
